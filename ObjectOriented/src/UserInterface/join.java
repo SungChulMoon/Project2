4ba897fb51f378023c05c68496c44688014a3ae1
PC <PC@DESKTOP-511ORRA>
2017-12-05 21:32:12 +0900

    회원가입수정

--- ObjectOriented/src/UserInterface/join.java
+++ ObjectOriented/src/UserInterface/join.java
@@ -1,12 +1,9 @@
 package UserInterface;
+
 import javax.swing.*;
 import java.awt.event.*;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.awt.*;
-
-import javax.swing.border.EmptyBorder;
-
 import InnerClass.DBmethod;
 /**
  * 유저들의 정보를 입력받아 DB로 보내고 저장시키는 GUI클래스
@@ -22,18 +19,10 @@ public class join extends JFrame {
 	ImageIcon Icon_w1 = new ImageIcon("img/w_body1.png");
 	ImageIcon Icon_w2 = new ImageIcon("img/w_body2.png");
 	ImageIcon Icon_w3 = new ImageIcon("img/w_body3.png");
-	JRadioButton rd_body0 = new JRadioButton(Icon_w0, true);
-	JRadioButton rd_body1 = new JRadioButton(Icon_w1);
-	JRadioButton rd_body2 = new JRadioButton(Icon_w2);
-	JRadioButton rd_body3 = new JRadioButton(Icon_w3);
 	ImageIcon Icon_m0 = new ImageIcon("img/m_body0.png");
 	ImageIcon Icon_m1 = new ImageIcon("img/m_body1.png");
 	ImageIcon Icon_m2 = new ImageIcon("img/m_body2.png");
 	ImageIcon Icon_m3 = new ImageIcon("img/m_body3.png");
-	JRadioButton rd_m_body0 = new JRadioButton(Icon_m0, true);
-	JRadioButton rd_m_body1 = new JRadioButton(Icon_m1, true);
-	JRadioButton rd_m_body2 = new JRadioButton(Icon_m2, true);
-	JRadioButton rd_m_body3 = new JRadioButton(Icon_m3, true);
 	ImageIcon Icon_id = new ImageIcon("img/lb_join_id.png");
 	ImageIcon Icon_name = new ImageIcon("img/lb_join_name.png");
 	ImageIcon Icon_pass = new ImageIcon("img/lb_join_pass.png");
@@ -44,40 +33,43 @@ public class join extends JFrame {
 	ImageIcon Icon_back_join = new ImageIcon("img/lb_back_join.png");
 	ImageIcon Icon_btn_join_join = new ImageIcon("img/btn_join_join.png");
 	ImageIcon Icon_btn_check = new ImageIcon("img/btn_check.png");
+	
+	JRadioButton rd_body0 = new JRadioButton(Icon_w0, true);
+	JRadioButton rd_body1 = new JRadioButton(Icon_w1);
+	JRadioButton rd_body2 = new JRadioButton(Icon_w2);
+	JRadioButton rd_body3 = new JRadioButton(Icon_w3);
+	JRadioButton rd_m_body0 = new JRadioButton(Icon_m0, true);
+	JRadioButton rd_m_body1 = new JRadioButton(Icon_m1, true);
+	JRadioButton rd_m_body2 = new JRadioButton(Icon_m2, true);
+	JRadioButton rd_m_body3 = new JRadioButton(Icon_m3, true);
+	
 	ArrayList<String> arrID = new ArrayList<>();
+	
 	boolean checkID = false;
+	boolean checkjoin = false;
+	
 	private JPanel contentPane;
-	private JTextField tf_name_id;
-	private JTextField tf_name_join;
-	private JPasswordField tf_pass;
-	private JPasswordField tf_checkpass;
+	private JTextField tf_join_id;
+	private JTextField tf_join_name;
+	private JPasswordField tf_join_pass;
+	private JPasswordField tf_join_passcheck;
+	private JTextField tf_join_local;
 	public ArrayList<String> checkStly = new ArrayList<>();
 	public JCheckBox cb_style0 = new JCheckBox("시크");
 	public JCheckBox cb_style1 = new JCheckBox("큐티");
 	public JCheckBox cb_style2 = new JCheckBox("섹시");
 	public JCheckBox cb_style3 = new JCheckBox("빈티지");
-	private JTextField tf_local_join;
 	
-
-	/**
-	 * Launch the application.
-	 */
-
-
-	/**
-	 * Create the frame.
-	 */
 	public join() {
-		
+		setTitle("웨더리쉬");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(300, 100, 720, 800);
+		setResizable(false);
 		
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
 		Image img = toolkit.getImage("img/Icon.png");
 		setIconImage(img);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(300, 100, 720, 800);
-		setTitle("웨더리쉬");
-		setResizable(false);
-
+		
 		contentPane = new JPanel(){
 			public void paintComponent(Graphics g) {
 				Rectangle r = getVisibleRect();
@@ -86,154 +78,155 @@ public class join extends JFrame {
 				super.paintComponent(g);
 			}
 		};
-		contentPane.setBackground(Color.WHITE);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
-
-		JLabel lblNewLabel = new JLabel(Icon_id);
-		lblNewLabel.setBounds(120, 33, 128, 52);
-		lblNewLabel.setForeground(new Color(5, 97, 232));
-		contentPane.add(lblNewLabel);
-
-		JLabel label = new JLabel(Icon_name);
-		label.setForeground(new Color(5, 97, 232));
-		label.setBounds(120, 96, 128, 52);
-		contentPane.add(label);
-
-		JLabel label_1 = new JLabel(Icon_pass);
-		label_1.setForeground(new Color(5, 97, 232));
-		label_1.setBounds(120, 159, 128, 52);
-		contentPane.add(label_1);
-
-		JLabel label_2 = new JLabel(Icon_passcheck);
-		label_2.setForeground(new Color(5, 97, 232));
-		label_2.setBounds(120, 222, 155, 52);
-		contentPane.add(label_2);
-
-		JLabel label_3 = new JLabel(Icon_gender);
-		label_3.setForeground(new Color(5, 97, 232));
-		label_3.setBounds(135, 343, 100, 51);
-		contentPane.add(label_3);
-
-		JLabel label_5 = new JLabel(Icon_local);
-		label_5.setForeground(new Color(5, 97, 232));
-		label_5.setBounds(120, 287, 128, 52);
-		contentPane.add(label_5);
 		
-		tf_local_join = new JTextField();
-		tf_local_join.setColumns(10);
-		tf_local_join.setBounds(275, 295, 288, 37);
-		contentPane.add(tf_local_join);
-	
-		
-		tf_name_id = new JTextField();
-		tf_name_id.setBounds(275, 41, 288, 37);
-		contentPane.add(tf_name_id);
-		tf_name_id.setColumns(10);
+		JLabel lb_join_id = new JLabel(Icon_id);
+		JLabel lb_join_name = new JLabel(Icon_name);
+		JLabel lb_join_pass = new JLabel(Icon_pass);
+		JLabel lb_join_passcheck = new JLabel(Icon_passcheck);
+		JLabel lb_join_local = new JLabel(Icon_local);
+		JLabel lb_join_gender = new JLabel(Icon_gender);
+		JLabel lb_join_style = new JLabel(Icon_style);
+		JLabel lb_equal = new JLabel("불일치");
+		tf_join_id = new JTextField();
+		tf_join_name = new JTextField();
+		tf_join_pass = new JPasswordField();
+		tf_join_passcheck = new JPasswordField();
+		tf_join_local = new JTextField();
+		JRadioButton rd_woman = new JRadioButton("여자", true);
+		JRadioButton rd_man = new JRadioButton("남자");
+		JButton btn_back = new JButton(new ImageIcon("img/lb_back.png"));
+		JButton btn_join = new JButton(Icon_btn_join_join);
+		JButton btn_check = new JButton(Icon_btn_check);
 
-		tf_name_join = new JTextField();
-		tf_name_join.setColumns(10);
-		tf_name_join.setBounds(275, 100, 288, 37);
-		contentPane.add(tf_name_join);
-
-		tf_pass = new JPasswordField();
-		tf_pass.setColumns(10);
-		tf_pass.setBounds(275, 163, 288, 37);
-		contentPane.add(tf_pass);
-
-		tf_checkpass = new JPasswordField();
-		tf_checkpass.setColumns(10);
-		tf_checkpass.setBounds(275, 232, 288, 37);
-		contentPane.add(tf_checkpass);
-
-		ButtonGroup g = new ButtonGroup();
-		ButtonGroup g_woman = new ButtonGroup();
-		ButtonGroup g_man = new ButtonGroup();
-
-		JLabel label_4 = new JLabel(Icon_style);
-		label_4.setForeground(new Color(5, 97, 232));
-		label_4.setFont(new Font("나눔바른고딕", Font.PLAIN, 20));
-		label_4.setBounds(120, 568, 124, 51);
-		contentPane.add(label_4);
-
-		cb_style0.setFont(new Font("나눔바른고딕", Font.BOLD, 17));
-		cb_style0.setBackground(Color.WHITE);
-		cb_style0.setBounds(250, 581, 131, 27);
-		cb_style0.setForeground(new Color(5, 97, 232));
-		contentPane.add(cb_style0);
-
-		cb_style1.setFont(new Font("나눔바른고딕", Font.BOLD, 17));
-		cb_style1.setForeground(new Color(5, 97, 232));
-		cb_style1.setBackground(Color.WHITE);
-		cb_style1.setBounds(429, 581, 131, 27);
-		contentPane.add(cb_style1);
-
-		cb_style2.setFont(new Font("나눔바른고딕", Font.BOLD, 17));
-		cb_style2.setForeground(new Color(5, 97, 232));
-		cb_style2.setBackground(Color.WHITE);
-		cb_style2.setBounds(250, 633, 131, 27);
-		contentPane.add(cb_style2);
-
-		cb_style3.setFont(new Font("나눔바른고딕", Font.BOLD, 17));
-		cb_style3.setForeground(new Color(5, 97, 232));
-		cb_style3.setBackground(Color.WHITE);
-		cb_style3.setBounds(429, 633, 131, 27);
-		contentPane.add(cb_style3);
 
 		rd_body0.addItemListener(new bodyItemcheck());
 		rd_body1.addItemListener(new bodyItemcheck());
 		rd_body2.addItemListener(new bodyItemcheck());
 		rd_body3.addItemListener(new bodyItemcheck());
-
 		rd_m_body0.addItemListener(new bodyItemcheck());
 		rd_m_body1.addItemListener(new bodyItemcheck());
 		rd_m_body2.addItemListener(new bodyItemcheck());
 		rd_m_body3.addItemListener(new bodyItemcheck());
-		rd_body0.setBackground(Color.WHITE);
-		rd_body0.setBorderPainted(true);
+		cb_style0.addItemListener(new itemListen());
+		cb_style1.addItemListener(new itemListen());
+		cb_style2.addItemListener(new itemListen());
+		cb_style3.addItemListener(new itemListen());
+		ButtonGroup g = new ButtonGroup();
+		ButtonGroup g_woman = new ButtonGroup();
+		ButtonGroup g_man = new ButtonGroup();
+
+		lb_join_id.setBounds(120, 33, 128, 52);
+		lb_join_name.setBounds(120, 96, 128, 52);
+		lb_join_pass.setBounds(120, 159, 128, 52);
+		lb_join_passcheck.setBounds(120, 222, 155, 52);
+		lb_join_local.setBounds(120, 287, 128, 52);
+		lb_join_gender.setBounds(135, 343, 100, 51);
+		lb_join_style.setBounds(120, 568, 124, 51);
+		tf_join_id.setBounds(275, 41, 288, 37);
+		tf_join_name.setBounds(275, 100, 288, 37);
+		tf_join_pass.setBounds(275, 163, 288, 37);
+		tf_join_passcheck.setBounds(275, 232, 288, 37);
+		tf_join_local.setBounds(275, 295, 288, 37);
+		cb_style0.setBounds(250, 581, 131, 27);
+		cb_style1.setBounds(429, 581, 131, 27);
+		cb_style2.setBounds(250, 633, 131, 27);
+		cb_style3.setBounds(429, 633, 131, 27);
 		rd_body0.setBounds(252, 406, 75, 150);
-
-		contentPane.add(rd_body0);
-
-		rd_body1.setBackground(Color.WHITE);
-		rd_body1.setBorderPainted(true);
 		rd_body1.setBounds(341, 406, 75, 150);
-		contentPane.add(rd_body1);
-
-		rd_body2.setBackground(Color.WHITE);
-		rd_body2.setBorderPainted(true);
 		rd_body2.setBounds(430, 406, 75, 150);
-		contentPane.add(rd_body2);
-
-		rd_body3.setBackground(Color.WHITE);
-		rd_body3.setBorderPainted(true);
 		rd_body3.setBounds(519, 406, 90, 150);
+		rd_m_body0.setBounds(252, 406, 75, 150);
+		rd_m_body1.setBounds(341, 406, 75, 150);
+		rd_m_body2.setBounds(430, 406, 75, 150);
+		rd_m_body3.setBounds(519, 406, 90, 150);
+		rd_woman.setBounds(250, 355, 93, 27);
+		rd_man.setBounds(429, 356, 93, 27);
+		btn_join.setBounds(120, 675, 440, 51);
+		btn_back.setBounds(33, 33, 51, 51);
+		btn_check.setBounds(580, 35, 105, 40);
+		
+		tf_join_id.setColumns(10);
+		tf_join_name.setColumns(10);
+		tf_join_pass.setColumns(10);
+		tf_join_passcheck.setColumns(10);
+		tf_join_local.setColumns(10);
+		
+		contentPane.add(lb_join_id);
+		contentPane.add(lb_join_name);
+		contentPane.add(lb_join_pass);
+		contentPane.add(lb_join_passcheck);
+		contentPane.add(lb_join_local);
+		contentPane.add(lb_join_gender);
+		contentPane.add(lb_join_style);
+		contentPane.add(tf_join_id);
+		contentPane.add(tf_join_name);
+		contentPane.add(tf_join_pass);
+		contentPane.add(tf_join_passcheck);
+		contentPane.add(tf_join_local);
+
+
+
+		cb_style0.setFont(new Font("고딕", Font.BOLD, 17));
+		cb_style1.setFont(new Font("고딕", Font.BOLD, 17));
+		cb_style2.setFont(new Font("고딕", Font.BOLD, 17));
+		cb_style3.setFont(new Font("고딕", Font.BOLD, 17));
+		rd_woman.setFont(new Font("고딕", Font.PLAIN, 20));
+		rd_man.setFont(new Font("굴림", Font.PLAIN, 20));
+		
+		cb_style0.setBackground(Color.WHITE);
+		cb_style1.setBackground(Color.WHITE);
+		cb_style2.setBackground(Color.WHITE);
+		cb_style3.setBackground(Color.WHITE);
+		rd_body0.setBackground(Color.WHITE);
+		rd_body1.setBackground(Color.WHITE);
+		rd_body2.setBackground(Color.WHITE);
+		rd_body3.setBackground(Color.WHITE);
+		rd_m_body0.setBackground(Color.WHITE);
+		rd_m_body1.setBackground(Color.WHITE);
+		rd_m_body2.setBackground(Color.WHITE);
+		rd_m_body3.setBackground(Color.WHITE);
+		rd_woman.setBackground(Color.WHITE);
+		rd_man.setBackground(Color.WHITE);
+		btn_back.setBackground(Color.WHITE);
+		
+		cb_style0.setForeground(new Color(5, 97, 232));
+		cb_style1.setForeground(new Color(5, 97, 232));
+		cb_style2.setForeground(new Color(5, 97, 232));
+		cb_style3.setForeground(new Color(5, 97, 232));
+		rd_woman.setForeground(new Color(5, 97, 232));
+		rd_man.setForeground(new Color(5, 97, 232));
+		lb_equal.setForeground(Color.RED);
+		
+		
+
+		rd_body0.setBorderPainted(true);
+		rd_body1.setBorderPainted(true);
+		rd_body2.setBorderPainted(true);
+		rd_body3.setBorderPainted(true);
+		rd_m_body0.setBorderPainted(true);
+		rd_m_body1.setBorderPainted(true);
+		rd_m_body2.setBorderPainted(true);
+		rd_m_body3.setBorderPainted(true);
+		contentPane.add(cb_style0);
+		contentPane.add(cb_style1);
+		contentPane.add(cb_style2);
+		contentPane.add(cb_style3);
+		contentPane.add(rd_body0);
+		contentPane.add(rd_body1);
+		contentPane.add(rd_body2);
 		contentPane.add(rd_body3);
+
+
 		g_woman.add(rd_body0);
 		g_woman.add(rd_body1);
 		g_woman.add(rd_body2);
 		g_woman.add(rd_body3);
 
-		rd_m_body0.setBackground(Color.WHITE);
-		rd_m_body0.setBorderPainted(true);
-		rd_m_body0.setBounds(252, 406, 75, 150);
 		contentPane.add(rd_m_body0);
-
-		rd_m_body1.setBorderPainted(true);
-		rd_m_body1.setBackground(Color.WHITE);
-		rd_m_body1.setBounds(341, 406, 75, 150);
 		contentPane.add(rd_m_body1);
-
-		rd_m_body2.setFont(new Font("나눔바른고딕", Font.PLAIN, 18));
-		rd_m_body2.setBackground(Color.WHITE);
-		rd_m_body2.setBorderPainted(true);
-		rd_m_body2.setBounds(430, 406, 75, 150);
 		contentPane.add(rd_m_body2);
-
-		rd_m_body3.setBackground(Color.WHITE);
-		rd_m_body3.setBorderPainted(true);
-		rd_m_body3.setBounds(519, 406, 90, 150);
 		contentPane.add(rd_m_body3);
 		g_man.add(rd_m_body0);
 		g_man.add(rd_m_body1);
@@ -243,7 +236,6 @@ public class join extends JFrame {
 		rd_m_body1.setVisible(false);
 		rd_m_body2.setVisible(false);
 		rd_m_body3.setVisible(false);
-		JRadioButton rd_woman = new JRadioButton("\uC5EC\uC131", true);
 		rd_woman.addItemListener(new ItemListener() {
 
 			@Override
@@ -264,13 +256,8 @@ public class join extends JFrame {
 
 			}
 		});
-		rd_woman.setFont(new Font("나눔바른고딕", Font.PLAIN, 20));
-		rd_woman.setForeground(new Color(5, 97, 232));
-		rd_woman.setBackground(Color.WHITE);
-		rd_woman.setBounds(250, 355, 93, 27);
 		contentPane.add(rd_woman);
 
-		JRadioButton rd_man = new JRadioButton("\uB0A8\uC131");
 		rd_man.addItemListener(new ItemListener() {
 
 			@Override
@@ -292,52 +279,43 @@ public class join extends JFrame {
 
 			}
 		});
-		rd_man.setBackground(Color.WHITE);
-		rd_man.setFont(new Font("나눔바른고딕", Font.PLAIN, 20));
-		rd_man.setForeground(new Color(5, 97, 232));
-		rd_man.setBounds(429, 356, 93, 27);
 		contentPane.add(rd_man);
 		g.add(rd_man);
 		g.add(rd_woman);
 
-		JLabel jl_equal = new JLabel("불일치");
-		jl_equal.setFont(new Font("나눔바른고딕", Font.PLAIN, 18));
-		jl_equal.setForeground(Color.RED);
-		jl_equal.setBounds(588, 239, 62, 18);
-		contentPane.add(jl_equal);
-		boolean checkjoin = false;
-		JButton btn_join = new JButton(Icon_btn_join_join);
+		lb_equal.setFont(new Font("고딕", Font.PLAIN, 18));
+		contentPane.add(lb_equal);
 		btn_join.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				if (tf_name_id.getText().equals("")) {
+				if (tf_join_id.getText().equals("")) {
 					JOptionPane.showMessageDialog(null, "아이디를 작성해주세요", "Error", JOptionPane.ERROR_MESSAGE);
 				} else if (!checkID) {
 					JOptionPane.showMessageDialog(null, "아이디 중복검사 해주세요.", "Error", JOptionPane.ERROR_MESSAGE);
-				} else if (tf_name_join.getText().equals("")) {
+				} else if (tf_join_name.getText().equals("")) {
 					JOptionPane.showMessageDialog(null, "이름을 작성해주세요", "Error", JOptionPane.ERROR_MESSAGE);
-				} else if (tf_pass.getText().equals("")) {
+				} else if (tf_join_pass.getText().equals("")) {
 					JOptionPane.showMessageDialog(null, "비밀번호를 작성해주세요", "Error", JOptionPane.ERROR_MESSAGE);
-				} else if(tf_local_join.getText().equals("")) {
+				} else if(tf_join_local.getText().equals("")) {
 				
 					JOptionPane.showMessageDialog(null, "지역를 작성해주세요", "Error", JOptionPane.ERROR_MESSAGE);
 				} else if ((!cb_style0.isSelected()) && (!cb_style1.isSelected()) && (!cb_style2.isSelected())
 						&& (!cb_style3.isSelected())) {
 					JOptionPane.showMessageDialog(null, "스타일을 하나라도 선택해주세요", "Error", JOptionPane.ERROR_MESSAGE);
 				} else {
-					if (tf_pass.getText().equals(tf_checkpass.getText())) {
-						if(tf_local_join.getText().equals("광주")) {
+					if (tf_join_pass.getText().equals(tf_join_passcheck.getText())) {
+						if(tf_join_local.getText().equals("광주")) {
 							location ="Gwangju";
 						}
-						else if(tf_local_join.getText().equals("서울")) {
+						else if(tf_join_local.getText().equals("서울")) {
 							location = "Seoul";
 						}
-						else if(tf_local_join.getText().equals("부산")) {
+						else if(tf_join_local.getText().equals("부산")) {
 							location ="Busan";
 						}
-						jl_equal.setText("일치");
-						jl_equal.setForeground(Color.GREEN);
+						lb_equal.setText("일치");
+						lb_equal.setForeground(Color.GREEN);
 						String style_str1 = "";
 						for (int i = 0; i < checkStly.size(); i++) {
 							style_str1 += "," + checkStly.get(i);
@@ -346,7 +324,7 @@ public class join extends JFrame {
 						System.out.println(style_str);
 						try {
 							System.out.println(gender);
-							if (DBmethod.addUserInfo(tf_name_id.getText(), tf_pass.getText(), tf_name_join.getText(),
+							if (DBmethod.addUserInfo(tf_join_id.getText(), tf_join_pass.getText(), tf_join_name.getText(),
 									location,gender, checkbody, style_str, Integer.toString(cnt)) > 0) {
 								JOptionPane.showMessageDialog(null, "회원가입이 완료되었습니다.", "회원가입이 완료되었습니다.",
 										JOptionPane.INFORMATION_MESSAGE);
@@ -361,20 +339,16 @@ public class join extends JFrame {
 						}
 						// 체크박스 체크된 갯수와 체크된 스트링을 얻어서 ,로 붙여서 디비에 추가
 					} else {
-						jl_equal.setText("불일치");
-						jl_equal.setForeground(Color.RED);
+						lb_equal.setText("불일치");
+						lb_equal.setForeground(Color.RED);
 						JOptionPane.showMessageDialog(null, "확인 비밀번호가 불일치 입니다.", "Error", JOptionPane.ERROR_MESSAGE);
 					}
 				}
 			}
 		});
 
-		btn_join.setForeground(new Color(5, 97, 232));
-		btn_join.setFont(new Font("나눔바른고딕", Font.PLAIN, 19));
-		btn_join.setBounds(120, 675, 440, 51);
 		contentPane.add(btn_join);
 
-		JButton btn_check = new JButton(Icon_btn_check);
 		btn_check.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				try {
@@ -384,7 +358,7 @@ public class join extends JFrame {
 					e.printStackTrace();
 				}
 
-				if (arrID.contains(tf_name_id.getText())) {
+				if (arrID.contains(tf_join_id.getText())) {
 					checkID = false;
 
 				} else {
@@ -400,20 +374,10 @@ public class join extends JFrame {
 				}
 			}
 		});
-		btn_check.setFont(new Font("나눔바른고딕", Font.PLAIN, 18));
-		btn_check.setBounds(570, 46, 105, 27);
-		btn_check.setForeground(new Color(5, 97, 232));
 		contentPane.add(btn_check);
 		
 	
-		cb_style0.addItemListener(new itemListen());
-		cb_style1.addItemListener(new itemListen());
-		cb_style2.addItemListener(new itemListen());
-		cb_style3.addItemListener(new itemListen());
 		System.out.println(cnt + "");
-		JButton btn_back = new JButton(new ImageIcon("img/lb_back.png"));
-		btn_back.setBackground(Color.WHITE);
-		btn_back.setBounds(33, 33, 51, 51);
 		contentPane.add(btn_back);
 		btn_back.addActionListener(new ActionListener() {
 			@Override
